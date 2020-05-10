612290aaec21a9c8e2b82cd69979f7455d8a90d9
James Hall-Treworgy <jameshall-treworgy@Jamess-MacBook-Pro.local>
2020-05-09 21:50:07 -0400

    Add public folder.

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('*', (req, res) => res.sendFile(__dirname + '/dist/' + req.url))
+app.get('*', (req, res) => res.sendFile(__dirname + '/public/' + req.url))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
